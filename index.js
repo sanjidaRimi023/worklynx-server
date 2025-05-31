2961c7252097baf6602f82e04e557013037180fa
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-05-31 12:12:22 +0600

    sort tasks data using limit operation

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster23.vxjxadf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster23`;
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,14 +20,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
 
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
-  } finally {
-    // await client.close();
+    const taskCollection = client.db("workLynxDB").collection("tasks");
+
+    app.post("/tasks", async (req, res) => {
+      const newTasks = req.body;
+
+      const result = await taskCollection.insertOne(newTasks).find({}).sort;
+      res.send(result);
+    });
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection
+        .find({})
+        .sort({ deadline: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    // await client.close()
+    console.error('Failed to fetch tasks:', error);
+    res.status(500).json({ error: 'Failed to fetch tasks' });
   }
 }
 run().catch(console.dir);
